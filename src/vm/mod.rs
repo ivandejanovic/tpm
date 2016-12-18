4d242fbe0d21a765063e1abe7d6a548a24537508
Ivan Dejanovic <idejanovic@gmail.com>
2016-12-18 12:39:40 +0100

    Added handlers for vm instructions.

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -24,63 +24,38 @@ use std::collections::HashMap;
 
 use common::INSTRUCTIONS;
 
-const OPCODE: Opcode = Opcode {
-    halt: 0x00,
-    in_n: 0x01,
-    out_n: 0x02,
-    in_s: 0x03,
-    out_s: 0x04,
-    add: 0x05,
-    sub: 0x06,
-    mul: 0x07,
-    div: 0x08,
-    con: 0x09,
-    push: 0x0A,
-    pop: 0x0B,
-    ld: 0x0C,
-    st: 0x0D,
-    jmp: 0x0E,
-    jgr: 0x0F,
-    jge: 0x10,
-    jeq: 0x11,
-    jne: 0x12,
-    jle: 0x13,
-    jls: 0x14,
-};
+// Opcodes
+const HALT: i64 = 0x00;
+const IN_N: i64 = 0x01;
+const OUT_N: i64 = 0x02;
+const IN_S: i64 = 0x03;
+const OUT_S: i64 = 0x04;
+const ADD: i64 = 0x05;
+const SUB: i64 = 0x06;
+const MUL: i64 = 0x07;
+const DIV: i64 = 0x08;
+const CON: i64 = 0x09;
+const PUSH: i64 = 0x0A;
+const POP: i64 = 0x0B;
+const LD: i64 = 0x0C;
+const ST: i64 = 0x0D;
+const JMP: i64 = 0x0E;
+const JGR: i64 = 0x0F;
+const JGE: i64 = 0x10;
+const JEQ: i64 = 0x11;
+const JNE: i64 = 0x12;
+const JLE: i64 = 0x13;
+const JLS: i64 = 0x14;
 
-const reg_num: usize = 8;
-const ins_mem_cap: usize = 50000;
+const REG_NUM: usize = 8;
+const INS_MEM_CAP: usize = 50000;
 
 pub struct Vm {
-    pc: i64,
-    sp: i64,
+    pc: usize,
+    sp: usize,
     acc: i64,
-    reg: [i64; reg_num],
-    ins_mem: [i64; ins_mem_cap],
-}
-
-struct Opcode {
-    pub halt: i64,
-    pub in_n: i64,
-    pub out_n: i64,
-    pub in_s: i64,
-    pub out_s: i64,
-    pub add: i64,
-    pub sub: i64,
-    pub mul: i64,
-    pub div: i64,
-    pub con: i64,
-    pub push: i64,
-    pub pop: i64,
-    pub ld: i64,
-    pub st: i64,
-    pub jmp: i64,
-    pub jgr: i64,
-    pub jge: i64,
-    pub jeq: i64,
-    pub jne: i64,
-    pub jle: i64,
-    pub jls: i64,
+    reg: [i64; REG_NUM],
+    ins_mem: [i64; INS_MEM_CAP],
 }
 
 impl Vm {
@@ -89,8 +64,8 @@ impl Vm {
             pc: 0,
             sp: 0,
             acc: 0,
-            reg: [0; reg_num],
-            ins_mem: [0; ins_mem_cap],
+            reg: [0; REG_NUM],
+            ins_mem: [0; INS_MEM_CAP],
         })
     }
 
@@ -99,32 +74,104 @@ impl Vm {
 
         let mut opcode_map: HashMap<&'static str, i64> = HashMap::new();
 
-        opcode_map.insert(INSTRUCTIONS.halt, OPCODE.halt);
-        opcode_map.insert(INSTRUCTIONS.in_n, OPCODE.in_n);
-        opcode_map.insert(INSTRUCTIONS.out_n, OPCODE.out_n);
-        opcode_map.insert(INSTRUCTIONS.in_s, OPCODE.in_s);
-        opcode_map.insert(INSTRUCTIONS.out_s, OPCODE.out_s);
-        opcode_map.insert(INSTRUCTIONS.add, OPCODE.add);
-        opcode_map.insert(INSTRUCTIONS.sub, OPCODE.sub);
-        opcode_map.insert(INSTRUCTIONS.mul, OPCODE.mul);
-        opcode_map.insert(INSTRUCTIONS.div, OPCODE.div);
-        opcode_map.insert(INSTRUCTIONS.con, OPCODE.con);
-        opcode_map.insert(INSTRUCTIONS.push, OPCODE.push);
-        opcode_map.insert(INSTRUCTIONS.pop, OPCODE.pop);
-        opcode_map.insert(INSTRUCTIONS.ld, OPCODE.ld);
-        opcode_map.insert(INSTRUCTIONS.st, OPCODE.st);
-        opcode_map.insert(INSTRUCTIONS.jmp, OPCODE.jmp);
-        opcode_map.insert(INSTRUCTIONS.jgr, OPCODE.jgr);
-        opcode_map.insert(INSTRUCTIONS.jge, OPCODE.jge);
-        opcode_map.insert(INSTRUCTIONS.jeq, OPCODE.jeq);
-        opcode_map.insert(INSTRUCTIONS.jne, OPCODE.jne);
-        opcode_map.insert(INSTRUCTIONS.jle, OPCODE.jle);
-        opcode_map.insert(INSTRUCTIONS.jls, OPCODE.jls);
+        opcode_map.insert(INSTRUCTIONS.halt, HALT);
+        opcode_map.insert(INSTRUCTIONS.in_n, IN_N);
+        opcode_map.insert(INSTRUCTIONS.out_n, OUT_N);
+        opcode_map.insert(INSTRUCTIONS.in_s, IN_S);
+        opcode_map.insert(INSTRUCTIONS.out_s, OUT_S);
+        opcode_map.insert(INSTRUCTIONS.add, ADD);
+        opcode_map.insert(INSTRUCTIONS.sub, SUB);
+        opcode_map.insert(INSTRUCTIONS.mul, MUL);
+        opcode_map.insert(INSTRUCTIONS.div, DIV);
+        opcode_map.insert(INSTRUCTIONS.con, CON);
+        opcode_map.insert(INSTRUCTIONS.push, PUSH);
+        opcode_map.insert(INSTRUCTIONS.pop, POP);
+        opcode_map.insert(INSTRUCTIONS.ld, LD);
+        opcode_map.insert(INSTRUCTIONS.st, ST);
+        opcode_map.insert(INSTRUCTIONS.jmp, JMP);
+        opcode_map.insert(INSTRUCTIONS.jgr, JGR);
+        opcode_map.insert(INSTRUCTIONS.jge, JGE);
+        opcode_map.insert(INSTRUCTIONS.jeq, JEQ);
+        opcode_map.insert(INSTRUCTIONS.jne, JNE);
+        opcode_map.insert(INSTRUCTIONS.jle, JLE);
+        opcode_map.insert(INSTRUCTIONS.jls, JLS);
 
         for instruction in code {
             println!("{}", instruction);
         }
     }
 
-    pub fn execute(&self) {}
+    pub fn execute(&self) {
+        loop {
+            let inst = self.ins_mem[self.pc];
+
+            match inst {
+                HALT => break,
+                IN_N => self.process_in_n(),
+                OUT_N => self.process_out_n(),
+                IN_S => self.process_in_s(),
+                OUT_S => self.process_out_s(),
+                ADD => self.process_add(),
+                SUB => self.process_sub(),
+                MUL => self.process_mul(),
+                DIV => self.process_div(),
+                CON => self.process_con(),
+                PUSH => self.process_push(),
+                POP => self.process_pop(),
+                LD => self.process_ld(),
+                ST => self.process_st(),
+                JMP => self.process_jmp(),
+                JGR => self.process_jgr(),
+                JGE => self.process_jge(),
+                JEQ => self.process_jeq(),
+                JNE => self.process_jne(),
+                JLE => self.process_jle(),
+                JLS => self.process_jls(),
+                _ => {
+                    println!("Unknow instruction. Aborting.");
+                    break;
+                }
+            }
+        }
+    }
+
+    fn process_in_n(&self) {}
+
+    fn process_out_n(&self) {}
+
+    fn process_in_s(&self) {}
+
+    fn process_out_s(&self) {}
+
+    fn process_add(&self) {}
+
+    fn process_sub(&self) {}
+
+    fn process_mul(&self) {}
+
+    fn process_div(&self) {}
+
+    fn process_con(&self) {}
+
+    fn process_push(&self) {}
+
+    fn process_pop(&self) {}
+
+    fn process_ld(&self) {}
+
+    fn process_st(&self) {}
+
+    fn process_jmp(&self) {}
+
+    fn process_jgr(&self) {}
+
+    fn process_jge(&self) {}
+
+    fn process_jeq(&self) {}
+
+    fn process_jne(&self) {}
+
+    fn process_jle(&self) {}
+
+    fn process_jls(&self) {}
 }
